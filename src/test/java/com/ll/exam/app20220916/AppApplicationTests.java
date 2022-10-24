dafe03fc93155dc9dcaa3703e1f042ebb3e3df35
jhs512 <jangka512@gmail.com>
2022-10-24 09:12:36 +0900

    세팅

---
+++ src/test/java/com/ll/exam/app20220916/AppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ll.exam.app20220916;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
